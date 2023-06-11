d2b5733121740498f4afe503220234c0111660d6
mosetf <stanlym315@gmail.com>
2023-06-11 16:17:23 +0300

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
+    dup = my_list.copy()
     if idx < 0 or idx > len(my_list) - 1:
-        my_list[idx] = element
-        return my_list
+        return my_list.copy()
     else:
-        return my_list
+        dup[idx] = element
+        return dup
