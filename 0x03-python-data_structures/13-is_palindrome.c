7bf8938d53182d03a5aebe8b62d47a09d9358fe0
mosetf <stanlym315@gmail.com>
2023-06-12 21:03:46 +0300

    100-print_python_list_info.c

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,50 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks if a linked list is palindrome
+ * @head: head of the list
+ * Return: 0 if not, 1 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *current = *head, *prev, *nxt, *l_hed, *r_hed;
+	int list_len = 0, i = 0, not_p = 0;
+
+	if (*head == NULL || head == NULL)
+		return (1);
+	while (current != NULL)
+		list_len++, current = current->next;
+	if (list_len == 1)
+		return (1);
+	current = *head;
+	for (i = 1; i <= list_len / 2 && current != NULL; i++)
+	{
+		next = current->next;
+		if (prev != NULL)
+			current->next = prev;
+		else
+			current->next = NULL;
+		prev = current, current = next;
+	}
+	right_head = current, left_head = prev;
+	for (i = 1; i <= list_len / 2 && current != NULL; i++)
+	{
+		if (list_len % 2 != 0 && i == 1)
+			current = current->next;
+		if (current->n != prev->n)
+		{
+			not_p = 1;
+			break;
+		}
+		current = current->next, prev = prev->next;
+	}
+	current = left_head, prev = right_head;
+	for (i = 1; i <= list_len / 2 && current != NULL; i++)
+	{
+		next = current->next;
+		if (prev != NULL)
+			current->next = prev;
+		prev = current, current = next;
+	}
+	return (not_p == 1 ? 0 : 1);
+}
