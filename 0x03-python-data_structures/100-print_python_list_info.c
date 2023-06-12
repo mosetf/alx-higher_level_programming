0318af044ed450cb9a7300bf9f08cc86875984a7
mosetf <stanlym315@gmail.com>
2023-06-12 21:14:31 +0300

    100-print_python_list_info.c

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,21 +1,15 @@
-import ctypes
+#include <Python.h>
+#include <object.h>
+#include <listobject.h>
 
-lib = ctypes.CDLL('./libPyList.so')
-lib.print_python_list_info.argtypes = [ctypes.py_object]
-l = ['hello', 'World']
-lib.print_python_list_info(1)
-del l[1]
-lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
-lib.print_python_list_info(l)
-l = []
-lib.print_python_list_info(l)
-l.append(0)
-lib.print_python_list_info(l)
-l.append(1)
-l.append(2)
-l.append(3)
-l.append(4)
-lib.print_python_list_info(l)
-l.pop()
-lib.print_python_list_info(l)
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_Size(p);
+	int y;
+	PyListObject *obj = (PyListObject *)p;
+
+	printf("[*] Size of the Python List = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->alloacted);
+	for (y = 0; y < size; y++)
+		printf("Element %y: %s\n", y, Py_TYPE(obj->ob_item[y])->tp_name);
+}
