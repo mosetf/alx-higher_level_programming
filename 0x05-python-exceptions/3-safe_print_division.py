0601bbf035c1b726634c25c7dc8fef81292901b4
stan <119288936+mosetf@users.noreply.github.com>
2023-06-26 19:47:48 +0300

    Delete 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        x = a / b 
-    except Exception:
-        x = None
-    finally:
-        print("Inside result: {}".format(x))
-        return x
