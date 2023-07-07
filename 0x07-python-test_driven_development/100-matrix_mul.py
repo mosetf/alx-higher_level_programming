29d0b12c18d6cc1e85f88f50b511a10ec3dc84f9
stan <119288936+mosetf@users.noreply.github.com>
2023-07-07 19:07:22 +0300

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3 
- """matrix_mul 
- Multiplies two matrices and returns the result 
+ """Module matrix_mul 
+ Multiplies two matrices and returns the result. 
  """ 
   
   
  def matrix_mul(m_a, m_b): 
-     """Return the matrix resulting after
-     multiplication of m_a and m_b.""" 
+     """Return the matrix resulting of 
+     the multiplication of m_a and m_b.""" 
   
      if type(m_a) is not list: 
          raise TypeError("m_a must be a list") 
