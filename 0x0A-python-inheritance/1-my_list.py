3cdf2884a9276be6e2bbb249a29e788d2760915a
root <root@stan.localdomain>
2023-07-10 15:57:49 +0300

    fix

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -8,6 +8,6 @@ class MyList (list):
     """class mylist"""
     def print_sorted(self):
         """module that prints a list sorted in a ascending order"""
-        sort_ted = sorted(self)
-        for i in sort_ted:
-            print("{}".format(i))
+        sort_ted = self[:]
+        sort_ted.sort()
+        print("{}".format(sort_ted))
