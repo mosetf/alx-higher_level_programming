f22b0bd42c2a01d4866c71724697e962bc0b333b
root <root@stan.localdomain>
2023-07-10 14:09:45 +0300

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,10 +3,13 @@
 module that that returns the list of
 available attributes and methods of an object
 """
+
+
 def lookup(obj):
     """
     a funt that rerurns a list object
     args:
+
         - obj: object to be inspected
     """
     return dir(obj)
