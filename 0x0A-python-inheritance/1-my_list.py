edc930a72f147e15ca0a0b05b547dc24de8c9359
root <root@stan.localdomain>
2023-07-10 15:40:50 +0300

    1-my_list.py

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+class MyList that inherits from list
+"""
+
+
+class MyList (list):
+    """class mylist"""
+    def print_sorted(self):
+        """module that prints a list sorted in a ascending order"""
+        self.list.sort()
+        print(list)
