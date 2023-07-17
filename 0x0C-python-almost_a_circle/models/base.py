06892973130fd343a25baaafea41a81e25085a83
root <root@stan.localdomain>
2023-07-17 22:37:52 +0300

    modified base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 """ base module"""
+
+
 class Base:
-    """base” of all other classes in this project
-    arg:
-     id:has argument value
+    """base of all other classes in this project
     """
     __nb_objects = 0
+
     def __init__(self, id = None):
+        """initialization
+        Arg:
+            id:has argument value
+        """
         if id != None:
-              self.id = id
+            self.id = id
         else:
-               Base.__nb_objects +=1
-               self.id = Base.__nb_objects
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
